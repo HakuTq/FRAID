fed8165a3734860429ea9d667d6a4128ed180780
Kokorotko <prdak05@seznam.cz>
2024-06-20 00:44:21 +0200

    Save/Load Menu Finished

--- Assets/Scripts/UI/UI_SaveMenu.cs
+++ Assets/Scripts/UI/UI_SaveMenu.cs
@@ -5,6 +5,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using JetBrains.Annotations;
 
 public class UI_SaveMenu : MonoBehaviour
 {
@@ -21,43 +22,6 @@ public class UI_SaveMenu : MonoBehaviour
     bool[] saveEmpty;
     bool optionConfirmed = false;
     bool optionCanceled = false;
-    void Start()
-    {
-        foreach (GameObject a in newGameButton) a.SetActive(false);
-        saveEmpty = new bool[saveFileSelector.Count()];
-        saveSystem.DoesSaveFileExist(out saveEmpty[0], out saveEmpty[1], out saveEmpty[2]);
-        //3 ifs jednoho dne dat do cyklu
-        if (saveEmpty[0])
-        {
-            buttonText[0].text = "New Game";
-        }
-        else
-        {
-            saveSystem.CurrentSaveID = SaveSystem.SaveID.First;
-            buttonText[0].text = "Difficutly: " + saveSystem.CurrentDifficulty.ToString();
-            newGameButton[0].SetActive(true);
-        }
-        if (saveEmpty[1])
-        {
-            buttonText[1].text = "New Game";
-        }
-        else
-        {
-            saveSystem.CurrentSaveID = SaveSystem.SaveID.Second;
-            buttonText[1].text = "Difficutly: " + saveSystem.CurrentDifficulty.ToString();
-            newGameButton[1].SetActive(true);
-        }
-        if (saveEmpty[2])
-        {
-            buttonText[2].text = "New Game";
-        }
-        else
-        {
-            saveSystem.CurrentSaveID = SaveSystem.SaveID.Third;
-            buttonText[2].text = "Difficutly: " + saveSystem.CurrentDifficulty.ToString();
-            newGameButton[2].SetActive(true);
-        }
-    }
 
     public void Button01Clicked()
     {
@@ -122,57 +86,106 @@ public class UI_SaveMenu : MonoBehaviour
         }
     }
 
-    public void Button01NewGame()
+    public void Button01NewGame(bool optionAlreadySelected)
     {
         confirm.SetActive(true);
-        StartCoroutine(ConfirmWaiting(1));
-        if (optionConfirmed)
+        if (!optionAlreadySelected) StartCoroutine(ConfirmWaiting(1));
+        else if (optionConfirmed)
         {
             saveSystem.CurrentSaveID = SaveSystem.SaveID.First;
             saveSystem.ResetSave();
+            SceneManager.LoadScene(saveSystem.CurrentScene.ToString());
         }
-        confirm.SetActive(false);
+        else if (optionCanceled) confirm.SetActive(false);
     }
 
-    public void Button02NewGame()
+    public void Button02NewGame(bool optionAlreadySelected)
     {
         confirm.SetActive(true);
-        StartCoroutine(ConfirmWaiting(2));
-        if (optionConfirmed)
+        if (!optionAlreadySelected) StartCoroutine(ConfirmWaiting(2));
+        else if (optionConfirmed)
         {
             saveSystem.CurrentSaveID = SaveSystem.SaveID.Second;
             saveSystem.ResetSave();
+            SceneManager.LoadScene(saveSystem.CurrentScene.ToString());
         }
-        confirm.SetActive(false);
+        else if (optionCanceled) confirm.SetActive(false);
     }
 
-    public void Button03NewGame()
+    public void Button03NewGame(bool optionAlreadySelected)
     {
         confirm.SetActive(true);
-        StartCoroutine(ConfirmWaiting(3));
-        if (optionConfirmed)
+        if (!optionAlreadySelected) StartCoroutine(ConfirmWaiting(3));
+        else if (optionConfirmed)
         {
             saveSystem.CurrentSaveID = SaveSystem.SaveID.Third;
             saveSystem.ResetSave();
+            SceneManager.LoadScene(saveSystem.CurrentScene.ToString());
         }
-        if (optionCanceled) confirm.SetActive(false);
+        else if (optionCanceled) confirm.SetActive(false);
     }
 
 
     IEnumerator ConfirmWaiting(int id)
     {
         while (!optionConfirmed && !optionCanceled) yield return null;
-        switch(id)
+        switch (id)
         {
-            case 1: Button01NewGame(); break;
-            case 2: Button02NewGame(); break;
-            case 3: Button03NewGame(); break;
+            case 1: Button01NewGame(true); break;
+            case 2: Button02NewGame(true); break;
+            case 3: Button03NewGame(true); break;
             default: break;
         }
+        optionCanceled = false;
+        optionConfirmed = false;
     }
+
+    public void OptionConfirm() { optionConfirmed = true; }
+    public void OptionCancel() { optionCanceled = true; }
 
     public void ShowSaveMenu()
     {
         saveMenu.SetActive(true);
+        CheckSavedGames();
     }
+
+    public void CheckSavedGames()
+    {
+        foreach (GameObject a in newGameButton) a.SetActive(false);
+        saveEmpty = new bool[saveFileSelector.Count()];
+        saveSystem.DoesSaveFileExist(out saveEmpty[0], out saveEmpty[1], out saveEmpty[2]);
+        //3 ifs jednoho dne dat do cyklu
+        if (saveEmpty[0])
+        {
+            buttonText[0].text = "New Game";
+        }
+        else
+        {
+            saveSystem.CurrentSaveID = SaveSystem.SaveID.First;
+            buttonText[0].text = "Difficutly: " + saveSystem.CurrentDifficulty.ToString();
+            newGameButton[0].SetActive(true);
+        }
+        if (saveEmpty[1])
+        {
+            buttonText[1].text = "New Game";
+        }
+        else
+        {
+            saveSystem.CurrentSaveID = SaveSystem.SaveID.Second;
+            buttonText[1].text = "Difficutly: " + saveSystem.CurrentDifficulty.ToString();
+            newGameButton[1].SetActive(true);
+        }
+        if (saveEmpty[2])
+        {
+            buttonText[2].text = "New Game";
+        }
+        else
+        {
+            saveSystem.CurrentSaveID = SaveSystem.SaveID.Third;
+            buttonText[2].text = "Difficutly: " + saveSystem.CurrentDifficulty.ToString();
+            newGameButton[2].SetActive(true);
+        }
+    }
+
+    public void CancelSaveMenu() { saveMenu.SetActive(false); }
 }
