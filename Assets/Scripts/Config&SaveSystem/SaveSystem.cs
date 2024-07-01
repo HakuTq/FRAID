6e2e7cf855273711a45462457417a9b70adab7ca
Kokorotko <prdak05@seznam.cz>
2024-07-01 12:13:30 +0200

    SaveFile Reworked

--- Assets/Scripts/Config&SaveSystem/SaveSystem.cs
+++ Assets/Scripts/Config&SaveSystem/SaveSystem.cs
@@ -2,23 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SaveSystem : MonoBehaviour
 {
+    //Variables for SaveSystem
     string fullPath;
-    private Difficulty currentDifficulty;
-    private SceneManagement currentScene;
     bool fileIsUsed = false;
 
-    public SceneManagement CurrentScene { get { return currentScene; } }
+    //private variables 
+    private Difficulty currentDifficulty;
+    private SceneManagement currentScene;
+    private SaveID currentSaveID;
 
+    //public variables
+    public SceneManagement CurrentScene { get { return currentScene; } private set { currentScene = value; } }
+    public Difficulty CurrentDifficulty
+    {
+        get { return currentDifficulty; }
+        set { currentDifficulty = value; }
+    }
+    public SaveID CurrentSaveID
+    {
+        get { return currentSaveID; }
+        set
+        {
+            if (value == SaveID.First) { fullPath = Path.Combine(Application.persistentDataPath, ("save01")); currentSaveID = value; }
+            else if (value == SaveID.Second) { fullPath = Path.Combine(Application.persistentDataPath, ("save02")); currentSaveID = value; }
+            else if (value == SaveID.Third) { fullPath = Path.Combine(Application.persistentDataPath, ("save03")); currentSaveID = value; }
+            else Debug.Log("CurrentSaveId is out of range");
+        }
+    }
+    //public Enums
     public enum SceneManagement
     {
+        Camp,
         Level_1,
         Level_1_Boss,
         Level_2,
-        Leve_2_Boss,
+        Level_2_Boss,
         Level_3,
         Level_3_Boss,
         Level_4,
@@ -30,35 +54,12 @@ public class SaveSystem : MonoBehaviour
         Normal,
         Hard
     }
-
-    public Difficulty CurrentDifficulty
-    {
-        get { return currentDifficulty; }
-        set { currentDifficulty = value; }
-    }
-
     public enum SaveID
     {
         First,
         Second,
         Third
     }
-
-    private SaveID currentSaveID;
-
-    public SaveID CurrentSaveID
-    {
-        get { return currentSaveID; }
-        set
-        {
-            if (value == SaveID.First) { fullPath = Path.Combine(Application.persistentDataPath, ("save01")); currentSaveID = value; }
-            else if (value == SaveID.Second) { fullPath = Path.Combine(Application.persistentDataPath, ("save02")); currentSaveID = value; }
-            else if (value == SaveID.Third) { fullPath = Path.Combine(Application.persistentDataPath, ("save03")); currentSaveID = value;}
-            else Debug.Log("CurrentSaveId is out of range");
-        }
-    }
-
-
     private void Start()
     {
         fullPath = Path.Combine(Application.persistentDataPath, ("save01"));
@@ -96,10 +97,100 @@ public class SaveSystem : MonoBehaviour
             using (FileStream file = new FileStream(fullPath, FileMode.OpenOrCreate, FileAccess.Write))
             {
                 BinaryWriter writer = new BinaryWriter(file);
-                if (CurrentDifficulty == Difficulty.Easy) writer.Write(01);
-                else if (CurrentDifficulty == Difficulty.Normal) writer.Write(02);
-                else if (CurrentDifficulty == Difficulty.Hard) writer.Write(03);
-                else Debug.Log("Nepodarilo se zapsat Current Difficulty do Save Filu SaveSystem/Save");
+                //Difficulty <diff:*value*>
+                switch (currentDifficulty)
+                {
+                    case Difficulty.Easy:
+                        {
+                            writer.Write("<diff:");
+                            writer.Write(00);
+                            writer.Write(">");
+                            break;
+                        }
+                    case Difficulty.Normal:
+                        {
+                            writer.Write("<diff:");
+                            writer.Write(01);
+                            writer.Write(">");
+                            break;
+                        }
+                    case Difficulty.Hard:
+                        {
+                            writer.Write("<diff:");
+                            writer.Write(02);
+                            writer.Write(">");
+                            break;
+                        }
+                    default: Debug.Log("Nepodarilo se zapsat 'currentDifficulty' do SaveFile " + currentSaveID); break;
+                }
+                //CurrentScene <Scene:*value*>
+                switch(currentScene)
+                {
+                    case SceneManagement.Camp:
+                        {
+                            writer.Write("<scene:");
+                            writer.Write(00);
+                            writer.Write(">");
+                            break;
+                        }
+                    case SceneManagement.Level_1:
+                        {
+                            writer.Write("<scene:");
+                            writer.Write(01);
+                            writer.Write(">");
+                            break;
+                        }
+                    case SceneManagement.Level_1_Boss:
+                        {
+                            writer.Write("<scene:");
+                            writer.Write(02);
+                            writer.Write(">");
+                            break;
+                        }
+                    case SceneManagement.Level_2:
+                        {
+                            writer.Write("<Scene:");
+                            writer.Write(03);
+                            writer.Write(">");
+                            break;
+                        }
+                    case SceneManagement.Level_2_Boss:
+                        {
+                            writer.Write("<scene:");
+                            writer.Write(04);
+                            writer.Write(">");
+                            break;
+                        }
+                    case SceneManagement.Level_3:
+                        {
+                            writer.Write("<scene:");
+                            writer.Write(05);
+                            writer.Write(">");
+                            break;
+                        }
+                    case SceneManagement.Level_3_Boss:
+                        {
+                            writer.Write("<Scene:");
+                            writer.Write(06);
+                            writer.Write(">");
+                            break;
+                        }
+                    case SceneManagement.Level_4:
+                        {
+                            writer.Write("<scene:");
+                            writer.Write(07);
+                            writer.Write(">");
+                            break;
+                        }
+                    case SceneManagement.Level_4_Boss:
+                        {
+                            writer.Write("<scene:");
+                            writer.Write(08);
+                            writer.Write(">");
+                            break;
+                        }
+                    default: Debug.Log("Could not save 'currentScene' from saveFile" + CurrentSaveID); break;
+                }
             }
             fileIsUsed = false;
         }
@@ -114,14 +205,44 @@ public class SaveSystem : MonoBehaviour
             using (FileStream file = new FileStream(fullPath, FileMode.OpenOrCreate, FileAccess.Read))
             {
                 BinaryReader reader = new BinaryReader(file);
-                int temp = reader.ReadInt32();
+                while (file.Position < file.Length)
+                {
+                    bool end = false;
+                    string variable;
+                    do
+                    {
+                        variable = reader.ReadChar().ToString();
+                        if (variable[0] == '<')
+                        {
+                            while (!variable.EndsWith(':')) variable += reader.ReadChar();
+                            switch (variable)
+                            {
+                                case "diff:":
+                                    {
+                                        int value = reader.ReadInt32();
+                                        currentDifficulty = (Difficulty)value;
+                                        break;
+                                    }
+                                case "scene:":
+                                    {
+                                        int value = reader.ReadInt32();
+                                        CurrentScene = (SceneManagement)value;
+                                        break;
+                                    }
+                                default: Debug.Log("Could not load variable"); break;
+                            }
+                        }
+                        else if (variable.EndsWith('>')) end = true;
+                    } while (!end);
+                }
+                /*int temp = reader.ReadInt32();
                 switch(temp)
                 {
                     case 1: CurrentDifficulty = Difficulty.Easy; break;
                     case 2: CurrentDifficulty = Difficulty.Normal; break;
                     case 3: currentDifficulty = Difficulty.Hard; break;
                     default: Debug.Log("Nelze nacist Current Difficulty v SaveSystemu/Load"); break;
-                }
+                }*/
             }
             fileIsUsed = false;
         }
